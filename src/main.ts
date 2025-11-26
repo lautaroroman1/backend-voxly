37b045fe8670c31e1c6c308c32ca1f302dcf98b7
Lautaro <lautaroroman138@gmail.com>
2025-11-26 08:09:47 -0300

    CORS

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:4200', 'https://frontend-voxly-lac.vercel.app'],
+    origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://localhost:4200', 'https://frontend-voxly-lac.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
